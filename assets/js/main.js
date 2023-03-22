d8877e5c09079f02ca40c7a10a1e63b07b76cab7
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-22 17:06:54 +0100

    add control repeat n generate and choice + print in page resut

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,30 +34,27 @@ Tools:
 
 */
 
-//definire un array che contenga i numeri generati in un secondo moment
-const numberDesk = [];
-
-//definire un array che contenga i  numeri scelti dall'utente
-const userNumberArray = [];
-
 //definisco una variabile per il conteggio dei numeri individuati
 let identifiedNumbers = 0;
 
 //creare una funzione che generi 5 numeri casuali
-function generateNumber(maxNumb) {
+function generateNumber() {
 
-    for (let i = 0; i < 5; i++) {
-        let numberGenerated = Math.floor(Math.random() * maxNumb) + 1;
-
-        //pushare i numeri nell'array
-        numberDesk.push(numberGenerated);
+    //definire un array che contenga i numeri generati
+    const numberDesk = []
+    while (numberDesk.length < 5) {
+        const numberGenerated = Math.floor(Math.random() * 100) + 1;
+        if(!numberDesk.includes(numberGenerated)){
+            //pushare i numeri nell'array
+            numberDesk.push(numberGenerated);
+        }
     }
+    return numberDesk
 
 }
 
-
-//scegliere il range massimo del numero casuale
-generateNumber(100);
+//salvare l'array con in numeri generati con la funzione in un array nel global scope
+const numberDesk = generateNumber()
 
 
 function loopArray(nameArray) {
@@ -68,12 +65,13 @@ function loopArray(nameArray) {
 
 }
 
-
 loopArray(numberDesk);
-
 
 //selezionare l'elemento in DOM dove stampare i numeri generati
 const h1Element = document.querySelector('.number_ghost');
+
+//selezionare l'elemento in DOM dove stampare i numeri scelti dall'utente
+const h2Element = document.querySelector('.number_choice');
 
 //stampare nell'elemento selezionato i numeri generati
 h1Element.insertAdjacentHTML('afterbegin', numberDesk);
@@ -82,69 +80,42 @@ h1Element.insertAdjacentHTML('afterbegin', numberDesk);
 setTimeout(removeElement, 30000);
 function removeElement() {
     h1Element.innerHTML = ``;
+
 }
-
-/*
-//selezionare il button nella DOM e salvarlo in una variabile
-const buttonEl = document.getElementById('submit');
-
-//creare un addEventListener per recuperare i valori degli input
-buttonEl.addEventListener('click', function(){
-    const number1 = document.getElementById('number1').value;
-    const number2 = document.getElementById('number2').value;
-    const number3 = document.getElementById('number3').value;
-    const number4 = document.getElementById('number4').value;
-    const number5 = document.getElementById('number5').value;
-
-    //passo i numeri scelti dall'utente all'array di riferimento
-    userNumberArray.push(number1, number2, number3, number4, number5);
-
-    console.log(userNumberArray);
-
-    //verificare se e quanti di questi numeri sono stati individuati
-    if (numberDesk.includes(number1)) {
-        console.log('il primo numero era presente');
-    } else if (numberDesk.includes(number2)){
-        console.log('il secondo numero era presente');
-    } else if (numberDesk.includes(number3)){
-        console.log('il terzo numero era presente');
-    } else if (numberDesk.includes(number4)){
-        console.log('il quarto numero era presente');
-    } else if (numberDesk.includes(number5)){
-        console.log('il quinto numero era presente');
-    } else {
-        console.log('il numero non era presente');
-    }
-
-    //indicare quanti e quali numeri sono stati individuati
-})
-*/
 
 //imposto un nuovo setTimeout così da far comparire i prompt per inserire i numeri dopo scomparsi quelli a schermo
 setTimeout(askNumber, 30100);
 function askNumber() {
-    //chiedo per 5 volte di inserire un numero
-    for (let i = 0; i < 5; i++) {
+        
+    //definire un array che contenga i  numeri scelti dall'utente
+    const userNumberArray = [];
+    //chiedo di inserire un numero finché non si scelgono 5 numeri diversi
+    while (userNumberArray.length < 5) {
         let userNumber = Number(prompt('Inserisci un numero'));
-        userNumberArray.push(userNumber);
-
-
+        //userNumberArray.push(userNumber);
+        
+        if (!userNumberArray.includes(userNumber)) {
+            userNumberArray.push(userNumber);
+        } else {
+            alert('Devi inserire un numero differente da quelli precedentemente inseriti')
+        }
+        
         //se il numero indicato è uguale ad uno dei numeri generati lo logghiamo in console e lo indichiamo all'utente
         if (numberDesk.includes(userNumber)) {
             console.log('Ha individuato il numero:', userNumber);
-
+            
             //aumento il conteggio dei numeri individuati
             identifiedNumbers++;
             
-
+            
             //altrimenti diciamo all'utente che il numero inserito non era presente
         } else {
             console.log('Mi dispiace il numero inserito non era presente');
         }
-
-        //mostro il conteggio dei numeri individuati
-        console.log('Hai inviduato:', identifiedNumbers , 'numeri');
+        
+        //mostro il conteggio dei numeri individuati in schermata
+        h2Element.innerHTML = `Hai inviduato: ${identifiedNumbers} numeri`;       
+        
     }
-
+    
 }
-
